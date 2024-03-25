bcd6299a059ee5b7eaf386bd7522adae38104232
aCodeDog <wang_zifan@outlook.com>
2024-03-25 14:18:24 +0800

    add

---
+++ legged_gym/envs/b2w/b2w_config.py
@@ -0,0 +1,184 @@
+# SPDX-FileCopyrightText: Copyright (c) 2021 NVIDIA CORPORATION & AFFILIATES. All rights reserved.
+# SPDX-License-Identifier: BSD-3-Clause
+# 
+# Redistribution and use in source and binary forms, with or without
+# modification, are permitted provided that the following conditions are met:
+#
+# 1. Redistributions of source code must retain the above copyright notice, this
+# list of conditions and the following disclaimer.
+#
+# 2. Redistributions in binary form must reproduce the above copyright notice,
+# this list of conditions and the following disclaimer in the documentation
+# and/or other materials provided with the distribution.
+#
+# 3. Neither the name of the copyright holder nor the names of its
+# contributors may be used to endorse or promote products derived from
+# this software without specific prior written permission.
+#
+# THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+# AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+# IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
+# DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE
+# FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL
+# DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR
+# SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER
+# CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY,
+# OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
+# OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+#
+# Copyright (c) 2021 ETH Zurich, Nikita Rudin
+import sys
+from legged_gym.envs.base.legged_robot_config import LeggedRobotCfg, LeggedRobotCfgPPO
+
+class B2wRoughCfg( LeggedRobotCfg ):
+    class env(LeggedRobotCfg.env):
+        num_envs = 6000
+        num_actions = 16
+        num_observations = 77 #+ 187
+    class commands( LeggedRobotCfg ):
+        curriculum = True
+        max_curriculum = 1.5
+        num_commands = 4 # default: lin_vel_x, lin_vel_y, ang_vel_yaw, heading (in heading mode ang_vel_yaw is recomputed from heading error)
+        resampling_time = 10. # time before command are changed[s]
+        heading_command = False # if true: compute ang vel command from heading error
+        class ranges:
+            lin_vel_x = [-1.0, 1.0] # min max [m/s]
+            lin_vel_y = [0.0, 0.0]   # min max [m/s]
+            ang_vel_yaw = [-0, 0]    # min max [rad/s]
+            heading = [-3.14, 3.14]
+    class terrain(LeggedRobotCfg.terrain):
+        mesh_type = 'plane' # "heightfield" # none, plane, heightfield or trimesh
+        horizontal_scale = 0.1 # [m]
+        vertical_scale = 0.005 # [m]
+        border_size = 25 # [m]
+        curriculum = True
+        static_friction = 1.0
+        dynamic_friction = 1.0
+        restitution = 0.
+        # rough terrain only:
+        measure_heights = True
+        measure_heights = False
+        measured_points_x = [-0.8, -0.7, -0.6, -0.5, -0.4, -0.3, -0.2, -0.1, 0., 0.1, 0.2, 0.3, 0.4, 0.5, 0.6, 0.7, 0.8] # 1mx1.6m rectangle (without center line)
+        measured_points_y = [-0.5, -0.4, -0.3, -0.2, -0.1, 0., 0.1, 0.2, 0.3, 0.4, 0.5]
+        selected = False # select a unique terrain type and pass all arguments
+        terrain_kwargs = None # Dict of arguments for selected terrain
+        max_init_terrain_level = 5 # starting curriculum state
+        terrain_length = 8.
+        terrain_width = 8.
+        num_rows= 10 # number of terrain rows (levels)
+        num_cols = 20 # number of terrain cols (types)
+        # terrain types: [smooth slope, rough slope, stairs up, stairs down, discrete]
+        terrain_proportions = [0.1, 0.1, 0.35, 0.25, 0.2]
+        # trimesh only:
+        slope_treshold = 0.75 # slopes above this threshold will be corrected to vertical surfaces
+   
+    class init_state( LeggedRobotCfg.init_state ):
+        pos = [0.0, 0.0, 0.5] # x,y,z [m]
+        default_joint_angles = { # = target angles [rad] when action = 0.0
+            'FL_hip_joint': 0.0,   # [rad]
+            'RL_hip_joint': 0.0,   # [rad]
+            'FR_hip_joint': 0.0 ,  # [rad]
+            'RR_hip_joint': 0.0,   # [rad]
+
+            'FL_thigh_joint': 0.67,     # [rad]
+            'RL_thigh_joint': 0.67,   # [rad]
+            'FR_thigh_joint': 0.67,     # [rad]
+            'RR_thigh_joint': 0.67,   # [rad]
+
+            'FL_calf_joint': -1.3,   # [rad]
+            'RL_calf_joint': -1.3,    # [rad]
+            'FR_calf_joint': -1.3,  # [rad]
+            'RR_calf_joint': -1.3,    # [rad]
+            'FL_foot_joint':0.0,
+            'RL_foot_joint':0.0,
+            'FR_foot_joint':0.0,
+            'RR_foot_joint':0.0,
+
+        }
+        init_joint_angles = { # = target angles [rad] when action = 0.0
+            'FL_hip_joint': 0.0,   # [rad]
+            'RL_hip_joint': 0.0,   # [rad]
+            'FR_hip_joint': 0.0 ,  # [rad]
+            'RR_hip_joint': 0.0,   # [rad]
+
+            'FL_thigh_joint': 0.67,     # [rad]
+            'RL_thigh_joint': 0.67,   # [rad]
+            'FR_thigh_joint': 0.67,     # [rad]
+            'RR_thigh_joint': 0.67,   # [rad]
+
+            'FL_calf_joint': -1.3,   # [rad]
+            'RL_calf_joint': -1.3,    # [rad]
+            'FR_calf_joint': -1.3,  # [rad]
+            'RR_calf_joint': -1.3,    # [rad]
+
+            'FL_foot_joint':0.0,
+            'RL_foot_joint':0.0,
+            'FR_foot_joint':0.0,
+            'RR_foot_joint':0.0,
+        }
+    class rewards:
+        class scales:
+            termination = -0.8
+            tracking_lin_vel = 3.0
+            tracking_ang_vel = 1.5
+            lin_vel_z = -0.1
+            ang_vel_xy = -0.05
+            orientation = -0.5
+            torques = -0.000001
+            dof_vel = 0
+            dof_acc = -1e-8
+            base_height = -0.5
+            feet_air_time =  1
+            collision = -0.1
+            feet_stumble = -0.
+            action_rate = -0.0002
+            stand_still = -0.01
+            joint_pos = -2
+            dof_pos_limits = -0.9
+            arm_pos = -0.
+            hip_action_l2 = -0.1
+
+
+
+        only_positive_rewards = True # if true negative total rewards are clipped at zero (avoids early termination problems)
+        tracking_sigma = 0.4 # tracking reward = exp(-error^2/sigma)
+        soft_dof_pos_limit = 0.9 # percentage of urdf limits, values above this limit are penalized
+        soft_dof_vel_limit = 0.9
+        soft_torque_limit = 1.
+        base_height_target = 0.34
+        max_contact_force = 100. # forces above this value are penalized
+
+    class control( LeggedRobotCfg.control ):
+        # PD Drive parameters:
+        control_type = 'P'
+        stiffness = {'joint': 300.,"fixed":10}  # [N*m/rad]
+        damping = {'joint': 15,"fixed":5}     # [N*m*s/rad]
+        # action scale: target angle = actionScale * action + defaultAngle
+        action_scale = 0.25
+        # decimation: Number of control action updates @ sim DT per policy DT
+        decimation = 4
+
+    class asset( LeggedRobotCfg.asset ):
+        file = '{LEGGED_GYM_ROOT_DIR}/resources/robots/b2w/urdf/b2w_description.urdf'
+        name = "aliengo"
+        foot_name = "foot"
+        wheel_name =["foot"]
+        penalize_contacts_on = ["thigh", "calf","base"]
+        terminate_after_contacts_on = []
+        self_collisions = 0 # 1 to disable, 0 to enable...bitwise filter "base","calf","hip","thigh"
+        replace_cylinder_with_capsule = False
+
+
+class B2wRoughCfgPPO( LeggedRobotCfgPPO ):
+    class algorithm( LeggedRobotCfgPPO.algorithm ):
+        entropy_coef = 0.01
+    class runner( LeggedRobotCfgPPO.runner ):
+        run_name = ''
+        experiment_name = 'b2w'
+        #resume = True
+        num_steps_per_env = 48 # per iteration
+        max_iterations = 3000
+        load_run = '/path/to/pretrained_model'
+        checkpoint = 0
+
+  

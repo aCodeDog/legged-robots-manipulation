dfbf775bc5e2d1de190b35836402dd88299ca7f1
WANG ZIFAN <94750458+aCodeDog@users.noreply.github.com>
2024-07-15 17:23:13 +0800

    add euler_from_quat function

--- legged_gym/envs/airbot/airbot_robot.py
+++ legged_gym/envs/airbot/airbot_robot.py
@@ -50,6 +50,29 @@ from legged_gym.utils.terrain import Terrain
 from legged_gym.utils.math import quat_apply_yaw, wrap_to_pi
 from legged_gym.utils.helpers import class_to_dict
 from .airbot_config import AirbotRoughCfg
+
+def euler_from_quat(quat_angle):
+    """
+    Convert a quaternion into euler angles (roll, pitch, yaw)
+    roll is rotation around x in radians (counterclockwise)
+    pitch is rotation around y in radians (counterclockwise)
+    yaw is rotation around z in radians (counterclockwise)
+    """
+    x = quat_angle[:,0]; y = quat_angle[:,1]; z = quat_angle[:,2]; w = quat_angle[:,3]
+    t0 = +2.0 * (w * x + y * z)
+    t1 = +1.0 - 2.0 * (x * x + y * y)
+    roll_x = torch.atan2(t0, t1)
+
+    t2 = +2.0 * (w * y - z * x)
+    t2 = torch.clip(t2, -1, 1)
+    pitch_y = torch.asin(t2)
+
+    t3 = +2.0 * (w * z + x * y)
+    t4 = +1.0 - 2.0 * (y * y + z * z)
+    yaw_z = torch.atan2(t3, t4)
+
+    return roll_x, pitch_y, yaw_z # in radians
+
 class Airbot(LeggedRobot):
     cfg:AirbotRoughCfg
 
